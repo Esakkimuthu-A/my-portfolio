453294c5d632a7dbc3229e2df76e24f58ce4437b
Esakkimuthu.A <mesakki834@gmail.com>
2025-06-15 20:27:16 +0530

    changes

--- src/app/auth/constants/portfolio.constant.ts
+++ src/app/auth/constants/portfolio.constant.ts
@@ -12,13 +12,13 @@ export const socialMediaIcons=[
 ]
 
 export const educationContent=[
-  {year:"2007 - 2019",content:"Government Higher Secondary School",aboutUs:"where I developed a strong foundation in problem-solving and technology. My curiosity and passion for coding led me to explore web development, and now I’m focused on building innovative digital products"},
-  {year:"2019 - 2023",content:"DR.Sivanthi Aditanar College Of Engineering",aboutUs:"I pursued my B.E. in Computer Science and Engineering at Dr. Sivanthi Aditanar College of Engineering, where I honed my technical skills and deepened my understanding of software development. College life was a transformative journey filled with hands-on projects, problem-solving, and collaboration with like-minded peers. It fueled my passion for coding and innovation, shaping me into the developer I am today"}
+  {year:"2007 - 2019",content:"Government Higher Secondary School",aboutUs:"This is where I developed a strong foundation in problem-solving and technology. My curiosity and passion for coding began here, leading me to explore web development. Since then, I’ve been focused on building innovative and user-friendly digital products."},
+  {year:"2019 - 2023",content:"DR.Sivanthi Aditanar College Of Engineering",aboutUs:"I completed my B.E. in Computer Science and Engineering at Dr. Sivanthi Aditanar College of Engineering. During this time, I strengthened my technical skills and gained a deeper understanding of software development. College life was a transformative journey—filled with hands-on projects, critical problem-solving, and collaboration with like-minded peers. It fueled my passion for coding and innovation, shaping me into the developer I am today."}
 ];
 
 export const experienceContent=[
-  {year:"2023 (6 - Months)",content:"Centizen (Internship)",aboutUs:"I also completed a 6-month internship at Centizen, where I gained hands-on experience in Angular, TypeScript, and Node.js. This internship helped me refine my skills in full-stack development and deepened my understanding of building scalable web applications."},
-  {year:"2023 - 2025",content:"Centizen.Incc",aboutUs:"I’m working at Centizen, where I focus on building scalable web applications using Angular, TypeScript, and Node.js. My role allows me to continuously explore new technologies and optimize performance for impactful digital solutions. Additionally, Im working on Zen-Studio, a platform designed to help users create their own websites effortlessly. I am always eager to learn, innovate, and take on new challenges in the ever-evolving tech landscape."}
+  {year:"2023 (6 - Months)",content:"Centizen – Internship",aboutUs:"I completed a 6-month internship at Centizen, where I gained practical experience with Angular, TypeScript, and Node.js. This opportunity helped me sharpen my full-stack development skills and deepened my understanding of building scalable and maintainable web applications."},
+  {year:"2023 - Present",content:"Centizen, Inc",aboutUs:"I currently work at Centizen, where I focus on building scalable and high-performance web applications using Angular, TypeScript, and Node.js. My role enables me to explore new technologies, enhance user experiences, and contribute to impactful digital solutions. I’m part of the team behind ZenBasket, a feature-rich e-commerce platform that helps businesses build and manage their online stores. I’m always eager to learn, innovate, and take on new challenges in the ever-evolving tech landscape."}
 ];
 
 export const contactAddress=[
