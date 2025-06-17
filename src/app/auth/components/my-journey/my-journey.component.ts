c1d7004f228a621001937026e455c7f3b65391aa
Esakkimuthu.A <mesakki834@gmail.com>
2025-06-17 14:07:39 +0530

    changes

--- src/app/auth/components/my-journey/my-journey.component.ts
+++ src/app/auth/components/my-journey/my-journey.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { educationContent,experienceContent } from '../../constants/portfolio.constant';
+import { educationContent, experienceContent } from '../../constants/portfolio.constant';
 
 @Component({
   selector: 'app-my-journey',
@@ -7,6 +7,58 @@ import { educationContent,experienceContent } from '../../constants/portfolio.co
   styleUrls: ['./my-journey.component.scss']
 })
 export class MyJourneyComponent {
+
+  animatedElements = new Set();
+  defaultAnimations: any = {
+    slideITopFade: [
+      { opacity: 0, transform: "translateY(20px)", offset: 0 },
+      { opacity: 1, transform: "translateY(0px)", offset: 1 }
+    ],
+    slideInLeftFade: [
+      { opacity: 0, transform: "translateX(100px)", offset: 0 },
+      { opacity: 1, transform: "translateX(0)", offset: 1 }
+    ],
+    slideInRightFade: [
+      { opacity: 0, transform: "translateX(-100px)", offset: 0 },
+      { opacity: 1, transform: "translateX(0)", offset: 1 }
+    ]
+  };
+  
   educationDetails = educationContent;
-  experienceDetails =experienceContent;
+  experienceDetails = experienceContent;
+
+  ngAfterViewInit() {
+    this.IntersectionObserver();
+  }
+
+  IntersectionObserver() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const el = entry.target;
+        const animationName = el.getAttribute("data-animation-name") || "slideInLeftFade";
+        const animateType = el.getAttribute("data-animation");
+        const animationTime = Number(el.getAttribute("data-animation-time")) || 700
+        if (entry.isIntersecting) {
+          this.animateElement(el, this.defaultAnimations[animationName], animationTime, "normal", 0);
+          this.animatedElements.add(el);
+        } else {
+          this.animateElement(el, this.defaultAnimations[animationName], animationTime, "reverse");
+        }
+      });
+    }, { threshold: 0.2 });
+    document.querySelectorAll("[data-animation='in'],[data-animation='in-out'],[data-animation='in-once']").forEach(el => {
+      observer.observe(el);
+    });
+  }
+
+  animateElement(elem: Element, animation: any, time: number, direction?: PlaybackDirection, delay = 0) {
+    return elem.animate(animation, {
+      delay,
+      duration: time,
+      direction,
+      easing: "ease-out",
+      iterations: 1,
+      fill: "forwards"
+    });
+  };
 }
